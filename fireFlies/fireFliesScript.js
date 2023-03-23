76cfa82c62fe233c2a9b139725e1ba56dd0e5917
rbbtz <69686526+rbbtz@users.noreply.github.com>
2023-03-23 03:26:12 -0400

    Update fireFliesScript.js

--- fireFlies/fireFliesScript.js
+++ fireFlies/fireFliesScript.js
@@ -30,49 +30,29 @@ function mouseClicked() {
 class Particle {
   constructor(x, y) {
     this.pos = createVector(x, y);
-    this.home = createVector(x, y);
     this.vel = createVector(random(-1, 1), random(-1, 1));
     this.acc = createVector();
     this.r = random(5, 30);
-    this.alpha = 100;
+    this.alpha = 50;
     this.hovering = false;
     this.clicked = false;
     this.color = color(220, 255, 170, 100);
-    this.flashOn = false;
-    this.flashTime = random(30, 120); // Random time for flash to stay on
-    this.flashTimer = this.flashTime; // Timer for the flash
-    this.maxSpeed = random(1, 4);
   }
 
   update() {
     if (this.hovering) {
-      let dir = p5.Vector.sub(this.pos, createVector(mouseX, mouseY));
-      dir.setMag(10);
-      this.acc.add(dir);
+      this.alpha = lerp(this.alpha, 255, 0.1);
+    } else {
+      this.alpha = lerp(this.alpha, 50, 0.1);
     }
     if (this.clicked) {
-      this.acc.add(createVector(random(-5, 5), random(-5, 5)));
-      this.alpha = lerp(this.alpha, 255, 0.1);
-    } else {
-      this.alpha = lerp(this.alpha, 100, 0.1);
+      let force = createVector(random(-5, 5), random(-5, 5));
+      this.acc.add(force);
+      this.vel.limit(10);
     }
     this.vel.add(this.acc);
-    this.vel.limit(this.maxSpeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
-
-    // Flashing effect
-    if (this.flashTimer > 0) {
-      this.flashTimer--;
-    } else {
-      this.flashTimer = this.flashTime;
-      this.flashOn = !this.flashOn;
-    }
-    if (this.flashOn) {
-      this.alpha = lerp(this.alpha, 255, 0.1);
-    } else {
-      this.alpha = lerp(this.alpha, 100, 0.1);
-    }
   }
 
   display() {
@@ -87,7 +67,6 @@ class Particle {
       this.hovering = true;
     } else {
       this.hovering = false;
-      this.vel.mult(0.95);
     }
   }
 
